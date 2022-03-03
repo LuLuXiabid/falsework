c1f55fa8bdf4aee42e3de2e8ebdf9a4a4c238ded
xiao.li <xiao.li@changhong.com>
2022-03-03 20:03:51 +0800

    feat:項目脚手架推動

---
+++ src/test/java/com/mcc/tem/falsework/FalseworkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mcc.tem.falsework;
+
+
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
